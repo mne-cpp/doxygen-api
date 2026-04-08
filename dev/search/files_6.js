8ec0ba40f0bb928601e2a598e7b8c117804b00e1
chdinh <christoph.dinh@mne-cpp.org>
2026-04-08 08:34:09 +0000

    Update dev API docs

--- dev/search/files_6.js
+++ dev/search/files_6.js
@@ -1,7 +1,7 @@
 var searchData=
 [
-  ['geometryinfo_2ecpp_0',['geometryinfo.cpp',['../a00140.html',1,'']]],
-  ['geometryinfo_2eh_1',['geometryinfo.h',['../a00134.html',1,'']]],
-  ['graph_2ecpp_2',['graph.cpp',['../a00620.html',1,'']]],
-  ['graph_2eh_3',['graph.h',['../a00632.html',1,'']]]
+  ['geometryinfo_2ecpp_0',['geometryinfo.cpp',['../a01787.html',1,'']]],
+  ['geometryinfo_2eh_1',['geometryinfo.h',['../a01790.html',1,'']]],
+  ['graph_2ecpp_2',['graph.cpp',['../a01133.html',1,'']]],
+  ['graph_2eh_3',['graph.h',['../a01115.html',1,'']]]
 ];
