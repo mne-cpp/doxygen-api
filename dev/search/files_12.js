1eb230af1181e69170792d689bcd6ed781a7bb17
chdinh <christoph.dinh@mne-cpp.org>
2026-04-09 14:08:12 +0000

    Update dev API docs

--- dev/search/files_12.js
+++ dev/search/files_12.js
@@ -1,11 +1,11 @@
 var searchData=
 [
-  ['tfplot_2ecpp_0',['tfplot.cpp',['../a00659.html',1,'']]],
-  ['tfplot_2eh_1',['tfplot.h',['../a00617.html',1,'']]],
-  ['tfsettingsview_2ecpp_2',['tfsettingsview.cpp',['../a00563.html',1,'']]],
-  ['tfsettingsview_2eh_3',['tfsettingsview.h',['../a00278.html',1,'']]],
-  ['timerulerwidget_2ecpp_4',['timerulerwidget.cpp',['../a00509.html',1,'']]],
-  ['timerulerwidget_2eh_5',['timerulerwidget.h',['../a00464.html',1,'']]],
-  ['triggerdetectionview_2ecpp_6',['triggerdetectionview.cpp',['../a00596.html',1,'']]],
-  ['triggerdetectionview_2eh_7',['triggerdetectionview.h',['../a00305.html',1,'']]]
+  ['tfplot_2ecpp_0',['tfplot.cpp',['../a01139.html',1,'']]],
+  ['tfplot_2eh_1',['tfplot.h',['../a01121.html',1,'']]],
+  ['tfsettingsview_2ecpp_2',['tfsettingsview.cpp',['../a01262.html',1,'']]],
+  ['tfsettingsview_2eh_3',['tfsettingsview.h',['../a01238.html',1,'']]],
+  ['timerulerwidget_2ecpp_4',['timerulerwidget.cpp',['../a01394.html',1,'']]],
+  ['timerulerwidget_2eh_5',['timerulerwidget.h',['../a01367.html',1,'']]],
+  ['triggerdetectionview_2ecpp_6',['triggerdetectionview.cpp',['../a01172.html',1,'']]],
+  ['triggerdetectionview_2eh_7',['triggerdetectionview.h',['../a01484.html',1,'']]]
 ];
--- dev/search/files_d.js
+++ dev/search/files_d.js
@@ -1,8 +1,8 @@
 var searchData=
 [
-  ['observerpattern_2ecpp_0',['observerpattern.cpp',['../a00731.html',1,'']]],
-  ['observerpattern_2eh_1',['observerpattern.h',['../a00737.html',1,'']]],
+  ['observerpattern_2ecpp_0',['observerpattern.cpp',['../a00827.html',1,'']]],
+  ['observerpattern_2eh_1',['observerpattern.h',['../a00842.html',1,'']]],
   ['overview_2emd_2',['overview.md',['../a01964.html',1,'']]],
-  ['overviewbarwidget_2ecpp_3',['overviewbarwidget.cpp',['../a00419.html',1,'']]],
-  ['overviewbarwidget_2eh_4',['overviewbarwidget.h',['../a00479.html',1,'']]]
+  ['overviewbarwidget_2ecpp_3',['overviewbarwidget.cpp',['../a01310.html',1,'']]],
+  ['overviewbarwidget_2eh_4',['overviewbarwidget.h',['../a01415.html',1,'']]]
 ];
