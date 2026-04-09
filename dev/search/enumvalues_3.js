35192519ff32e42dde00c8c65d9595c3ebc5ac98
chdinh <christoph.dinh@mne-cpp.org>
2026-04-09 16:04:24 +0000

    Update dev API docs

--- dev/search/enumvalues_3.js
+++ dev/search/enumvalues_3.js
@@ -4,7 +4,7 @@ var searchData=
   ['default_1',['Default',['../a02222.html#a36141ad3b6f880d2d82a3dd69d6e2e29a0f16170ebeafbe66f78cb2b8ac9f1151',1,'DISPLIB::AbstractView']]],
   ['dics_2',['DICS',['../a01990.html#a29bb5b206858a4db67919400558e5a99abca3775038822ce0be6483af819485f1',1,'INVLIB']]],
   ['digitizeritem_3',['DigitizerItem',['../a02546.html#a7804788c78ce642387ab26f50d3dda68ae90028ad306f3e932ba0d2d4e5d53282',1,'AbstractTreeItem']]],
-  ['dipole_4',['Dipole',['../a00077.html#a9a65336a07fa3c9b4b13e6d52e3a612ca5efa28befb847e237e1aacaa160743eb',1,'rendertypes.h']]],
+  ['dipole_4',['Dipole',['../a01727.html#a9a65336a07fa3c9b4b13e6d52e3a612ca5efa28befb847e237e1aacaa160743eb',1,'rendertypes.h']]],
   ['dipolebegin_5',['DipoleBegin',['../a01990.html#af1ffe934a77f18c6a7260b1975016387af17bcbeda3d74cccb6bab987fe37802e',1,'INVLIB']]],
   ['dipoleend_6',['DipoleEnd',['../a01990.html#af1ffe934a77f18c6a7260b1975016387a12bb8ba9ee00a029ff47d8c2e96de750',1,'INVLIB']]],
   ['dipolefit_7',['DipoleFit',['../a01990.html#a29bb5b206858a4db67919400558e5a99aaed2f72a37e53dedcf52fea2c57103ff',1,'INVLIB']]],
