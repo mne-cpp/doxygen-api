98c0b2e3376ffd2ba217b507c6a9b88639e9f64f
chdinh <christoph.dinh@mne-cpp.org>
2026-06-11 12:31:54 +0000

    Update dev API docs

--- dev/search/files_9.js
+++ dev/search/files_9.js
@@ -1,5 +1,5 @@
 var searchData=
 [
-  ['kmeans_2ecpp_0',['kmeans.cpp',['../a01064.html',1,'']]],
-  ['kmeans_2eh_1',['kmeans.h',['../a01040.html',1,'']]]
+  ['kmeans_2ecpp_0',['kmeans.cpp',['../a02132.html',1,'']]],
+  ['kmeans_2eh_1',['kmeans.h',['../a02117.html',1,'']]]
 ];
