cd259a7dcea5ff9b5af962a6a2b6e997f66cdf42
chdinh <christoph.dinh@mne-cpp.org>
2026-06-11 12:49:30 +0000

    Update dev API docs

--- dev/search/enumvalues_0.js
+++ dev/search/enumvalues_0.js
@@ -3,7 +3,7 @@ var searchData=
   ['abstractitem_0',['AbstractItem',['../a03166.html#a7804788c78ce642387ab26f50d3dda68aac38c1fb9e3c0035fdd08ce1af07ccb8',1,'AbstractTreeItem']]],
   ['alpharole_1',['AlphaRole',['../a03166.html#ae941378b7f6d564b5c34d9b68d1c4203afe4b2ae145bdc1d30dd51d78794ce91e',1,'AbstractTreeItem']]],
   ['amplitude_2',['Amplitude',['../a02558.html#af1ffe934a77f18c6a7260b1975016387af4d00f19181bdfc0474954a43262febf',1,'INVLIB']]],
-  ['anatomical_3',['Anatomical',['../a00983.html#a9a65336a07fa3c9b4b13e6d52e3a612caf3ba9c29e4c006591a31473ffac033fe',1,'rendertypes.h']]],
+  ['anatomical_3',['Anatomical',['../a00131.html#a9a65336a07fa3c9b4b13e6d52e3a612caf3ba9c29e4c006591a31473ffac033fe',1,'rendertypes.h']]],
   ['annotation_4',['Annotation',['../a02590.html#acec300107fc488331fcaddae5894abfca55f6611cb342565cf0f5002400f53a95',1,'MNALIB::Annotation'],['../a02590.html#aff2df5a82a2d98871c0ceb6dff9713f6a55f6611cb342565cf0f5002400f53a95',1,'MNALIB::Annotation']]],
   ['annotationbased_5',['AnnotationBased',['../a03350.html#a89820f353dedbdbee0c7af5b07d97663a751e8e87989929f912e7dc9e10b004b2',1,'DISP3DLIB::RtSourceInterpolationMatWorker']]],
   ['annotationdatarole_6',['AnnotationDataRole',['../a03198.html#a4428acbaaf56d9874c19e82819f3baa8a74d7e09c128a0af79481c7ba31744f80',1,'SurfaceTreeItem']]],
